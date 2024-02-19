07e4ac11ccce177b024de2897e80fae1ba65e8de
gokruzz <johnsonodukoyatemitope@gmail.com>
2024-02-19 15:15:25 -0800

    quick sort

--- sort.h
+++ sort.h
@@ -25,5 +25,7 @@ void insertion_sort_list(listint_t **list);
 void print_list(const listint_t *list);
 void swap_int(int *a, int *b);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+int lomuto_partition(int *array, size_t size, int l, int h);
 
 #endif
